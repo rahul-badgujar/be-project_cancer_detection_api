63286a8b0eb46e3b30e4e0389126bd7605c993b8
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2023-04-10 19:52:34 +0530

    v3 model tried

--- src/machine_learning/model/svm/cancer_stage_detection_model_v3.py
+++ src/machine_learning/model/svm/cancer_stage_detection_model_v3.py
@@ -16,8 +16,8 @@ from src.util.file_system_utils import FileSystemUtils
 
 class CancerStageDetectionModelV3:
     @staticmethod
-    def train():
-        RESIZE_DIMENS = (150, 150, 3)
+    def train(preprocess=True):
+        resize_dimension = (160, 160)
 
         training_images_paths = list(paths.list_images(AppConstants.training_dataset_directory))
         labels = [img_path.split(FileSystemUtils.get_os_path_separator())[-2] for img_path in training_images_paths]
@@ -25,9 +25,10 @@ class CancerStageDetectionModelV3:
         for img in training_images_paths:
             img_array = cv2.imread(img)
             img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
-            img_array = PreprocessingUtils.apply_all_preprocessors(img_array)
-            img_resized = resize(img_array, RESIZE_DIMENS)
-            input_features.append(img_resized.flatten())
+            img_array = resize(img_array, resize_dimension)
+            if preprocess:
+                img_array = PreprocessingUtils.apply_all_preprocessors(img_array)
+            input_features.append(img_array.flatten())
 
         flat_data = np.array(input_features)
         target = np.array(labels)
@@ -45,16 +46,12 @@ class CancerStageDetectionModelV3:
         print('Splitted Successfully')
         model.fit(x_train, y_train)
         print('The Model is trained well with the given images')
-        joblib.dump(model, "saved-model.pkl")
+        joblib.dump(model, f"saved-model_preprocess-{preprocess}.pkl")
 
         # Model testing
         y_pred = model.predict(x_test)
-        print("The predicted Data is :")
-        print(y_pred)
-        print("The actual data is:")
-        print(np.array(y_test))
         print(f"The model is {accuracy_score(y_pred, y_test) * 100}% accurate")
 
 
 if __name__ == '__main__':
-    CancerStageDetectionModelV3().train()
+    CancerStageDetectionModelV3.train(preprocess=True)
